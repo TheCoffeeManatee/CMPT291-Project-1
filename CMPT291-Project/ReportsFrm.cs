ebc0a3be8a1b5bed2eed3436b353292673247bdb
Haris Kajtazovic <kajtazovich@mymacewan.ca>
2022-03-08 10:42:56 -0700

    Made reports using tabControl, 2 reports will use user date input and 1 report will use user branch input and 2 reports will need no input

--- CMPT291-Project/ReportsFrm.cs
+++ CMPT291-Project/ReportsFrm.cs
@@ -16,5 +16,26 @@ namespace Lab6_Modern
         {
             InitializeComponent();
         }
+
+        private void ReportUpdate1_Click(object sender, EventArgs e)
+        {
+            string d1 = DatePick1.Value.ToString("yyyy/MM/dd");
+            string d2 = DatePick2.Value.ToString("yyyy/MM/dd");
+            Date1.Text = d1;
+            Date1.Visible = true;
+            Date2.Text = d2;
+            Date2.Visible = true;
+
+        }
+
+        private void UpdateReport2_Click(object sender, EventArgs e)
+        {
+            string d3 = DatePick3.Value.ToString("yyyy/MM/dd");
+            string d4 = DatePick4.Value.ToString("yyyy/MM/dd");
+            Date3.Text = d3;
+            Date3.Visible = true;
+            Date4.Text = d4;
+            Date4.Visible = true;
+        }
     }
 }
