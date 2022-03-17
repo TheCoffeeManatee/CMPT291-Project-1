7edbde6b191a1095633f08b90552420483297642
Coopmore <coopmore@LIVE.CA>
2022-03-17 11:00:27 -0600

    Database addition tables modified to actually put stuff in the database based on template from CarTypeEntry

--- CMPT291-Project/CarEntry.cs
+++ CMPT291-Project/CarEntry.cs
@@ -7,24 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CMPT291_Project
 {
     public partial class CarEntry : Form
     {
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+       
         public CarEntry()
         {
             InitializeComponent();
-        }
 
-        private void CarEntryTitle_Click(object sender, EventArgs e)
-        {
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
 
+            SqlConnection myConnection = new SqlConnection(connectionString);
+
+            try
+            {
+                myConnection.Open(); // Open connection
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection; // Link the command stream to the connection
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
         }
 
 
         private void carentrycancel_Click_1(object sender, EventArgs e)
         {
+            this.CarEntryPanel.Controls.Clear();
+            CarsFrm CarsFrm_Vrb = new CarsFrm() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            CarsFrm_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.CarEntryPanel.Controls.Add(CarsFrm_Vrb);
+            CarsFrm_Vrb.Show();
+        }
+
+        private void carentrryaccept_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myCommand.CommandText = "insert into Car values ('" + vinentry.Text +
+                    "','" + makeentry.Text + "','" + modelentry.Text + "'," + yearentry.Text + ","
+                    + seatsentry.Text + ",'" + colourentry.Text + "','" + insentry.Text + "'," + mileentry.Text + ","
+                    + branchentry.Text + "," + typeentry.Text + ")";
+                myCommand.ExecuteNonQuery();
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.ToString(), "Error");
+            }
+
             this.CarEntryPanel.Controls.Clear();
             CarsFrm CarsFrm_Vrb = new CarsFrm() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
             CarsFrm_Vrb.FormBorderStyle = FormBorderStyle.None;
--- CMPT291-Project/CustEntry.cs
+++ CMPT291-Project/CustEntry.cs
@@ -7,18 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CMPT291_Project
 {
     public partial class CustEntry : Form
     {
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+
         public CustEntry()
         {
             InitializeComponent();
+            
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+            SqlConnection myConnection = new SqlConnection(connectionString);
+
+            try
+            {
+                myConnection.Open(); // Open connection
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection; // Link the command stream to the connection
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
         }
 
         private void custentrycancel_Click(object sender, EventArgs e)
         {
+            this.CustEntryPanel.Controls.Clear();
+            CustomersFrm CustomersFrm_Vrb = new CustomersFrm() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            CustomersFrm_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.CustEntryPanel.Controls.Add(CustomersFrm_Vrb);
+            CustomersFrm_Vrb.Show();
+        }
+
+        private void custentryacc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myCommand.CommandText = "insert into Customer values ('" + fnameentry.Text +
+                    "','" + mnameentry.Text + "','" + lnameentry.Text + "','" + add1entry.Text + 
+                    "','" + add2entry.Text + "','" + cityentry.Text + "','" + proventry.Text + "','" +
+                    postalentry.Text + "','" + dobentry.Text + "'," + phoneentry.Text + ",'" + cinsentry.Text
+                    + "','" + dlentry.Text + "'," + mementry.Text + ")";
+                myCommand.ExecuteNonQuery();
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.ToString(), "Error");
+            }
+
             this.CustEntryPanel.Controls.Clear();
             CustomersFrm CustomersFrm_Vrb = new CustomersFrm() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
             CustomersFrm_Vrb.FormBorderStyle = FormBorderStyle.None;
