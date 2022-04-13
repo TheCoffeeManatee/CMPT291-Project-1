f097ba1c12f41c5e2b55d0ffccbcf4cb49a9f284
coopmore <101018387+coopmore@users.noreply.github.com>
2022-04-13 10:41:17 -0600

    commented unclear functions

--- CMPT291-Project/CarTypeFrm.cs
+++ CMPT291-Project/CarTypeFrm.cs
@@ -17,6 +17,7 @@ namespace CMPT291_Project
         public SqlConnection myConnection;
         public SqlCommand myCommand;
         public SqlDataReader myReader;
+        // state corresponds to the car type entry form state variable, and determines the actions taken on management form closure
         public int state = 0;
 
         public string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
@@ -26,6 +27,7 @@ namespace CMPT291_Project
         {
             InitializeComponent();
             myCommand = new SqlCommand("select * from CarType");
+            // this sequence changes state to allow the table to be populated with the entire table, then resets it
             state = 2;
             execute(); //fills datatable upon loading screen
             state = 0;
@@ -37,6 +39,7 @@ namespace CMPT291_Project
             myConnection = new SqlConnection(connectionString);
             myConnection.Open();
             myCommand.Connection = myConnection;
+            // this block determines, based on state, whether the tables should be edited, data grid populated, or no action taken
             if (state == 1)
             {
                 try
@@ -61,7 +64,8 @@ namespace CMPT291_Project
 
 
         }
-
+        
+        // this entire function just gets the table data from the server and formats the columns appropriately
         private void fillTable()
         {
             myCommand.CommandType = CommandType.Text;
@@ -84,7 +88,9 @@ namespace CMPT291_Project
             CarTypeData.Columns["Monthly Rate"].DefaultCellStyle.Format = "N2";
 
         }
-
+        
+        // this uses a dialog box to get the data from the user, and sets the sql command to the generated command from the management form
+        // and matches the state to the management forms state, then executes the appropriate command
         private void CarTypeAdd_Click_1(object sender, EventArgs e)
         {
             CarTypeEntry CarTypeEntry_Vrb = new CarTypeEntry() { TopLevel = true, TopMost = true };
