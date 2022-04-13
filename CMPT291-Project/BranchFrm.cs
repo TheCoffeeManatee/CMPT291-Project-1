1d98819752f6bdeeaae310ddafda1c6a3e7ad30a
coopmore <101018387+coopmore@users.noreply.github.com>
2022-04-13 10:37:46 -0600

    commented unclear functions

--- CMPT291-Project/BranchFrm.cs
+++ CMPT291-Project/BranchFrm.cs
@@ -18,6 +18,8 @@ namespace CMPT291_Project
         public SqlConnection myConnection;
         public SqlCommand myCommand;
         public SqlDataReader myReader;
+        // state corresponds to the entry form public variable state which is used to determine the operation performed
+        // upon closing the management form
         public int state = 0;
 
         public string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
@@ -27,6 +29,7 @@ namespace CMPT291_Project
         {
             InitializeComponent();
             myCommand = new SqlCommand("select * from Branch");
+            // the state is set to fill the tables, the tables are filled, and the state is changed back to perform no operations
             state = 2;
             execute(); //fills datatable upon loading screen
             state = 0;
@@ -37,6 +40,7 @@ namespace CMPT291_Project
             myConnection = new SqlConnection(connectionString);
             myConnection.Open();
             myCommand.Connection = myConnection;
+            // this block either edits the tables if the state is 1, or populates the display if it is 2, no action is taken if it is 0
             if (state == 1)
             {
                 try
@@ -55,7 +59,8 @@ namespace CMPT291_Project
             myConnection.Close();
 
         }
-
+        
+        // this is used to populate the display
         private void fillTable()
         {
             myCommand.CommandType = CommandType.Text;
@@ -74,11 +79,14 @@ namespace CMPT291_Project
 
         private void BranchManage_Click(object sender, EventArgs e)
         {
+            // the parameters here allow for the use of a management dialog box for easier screen data transfer
             BranchEntry BranchEntry_Vrb = new BranchEntry() { TopLevel = true, TopMost = true };
             BranchEntry_Vrb.FormBorderStyle = FormBorderStyle.None;
             BranchEntry_Vrb.ShowDialog();
+            // once the management form is closed, this forms sqlcommand is set to the generated command from the dialog box form, and the state is set to match as well
             myCommand.CommandText = BranchEntry_Vrb.newCommand;
             state = BranchEntry_Vrb.state;
+            // the new command is executed based on newly assigned management state
             execute();
         }
     }
