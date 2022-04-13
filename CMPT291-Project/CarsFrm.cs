52da08419d18e522a13f11a9c748d3b4c0d380bc
coopmore <101018387+coopmore@users.noreply.github.com>
2022-04-13 10:44:18 -0600

    commented unclear functions

--- CMPT291-Project/CarsFrm.cs
+++ CMPT291-Project/CarsFrm.cs
@@ -17,6 +17,7 @@ namespace CMPT291_Project
         public SqlConnection myConnection;
         public SqlCommand myCommand;
         public SqlDataReader myReader;
+        // state corresponds to a value in the car entry form which determines the operations performed here upon management form closure
         public int state = 0;
 
 
@@ -26,6 +27,7 @@ namespace CMPT291_Project
         {
             InitializeComponent();
             myCommand = new SqlCommand("select * from Car");
+            // the state is set to allow data view population, population is executed, then the state is reset to perform no actions
             state = 2;
             execute();
             state = 0;
@@ -36,6 +38,7 @@ namespace CMPT291_Project
             myConnection = new SqlConnection(connectionString);
             myConnection.Open();
             myCommand.Connection = myConnection;
+            // this block determines whether, based on state, the grid is populated, no action is taken, or the tables are edited
             if(state == 1)
             {
                 try
@@ -55,6 +58,7 @@ namespace CMPT291_Project
 
         }
 
+        // this function fills the data grid and formats the view accordingly
         private void fillTable()
         {
             myCommand.CommandType = CommandType.Text;
@@ -68,7 +72,9 @@ namespace CMPT291_Project
 
             carData.DataSource = dt;
         }
-
+        
+        // this function opens a dialog box for varied management functions, and takes the generated query and state from the dialog box management form
+        // and executes the appropriate actions on form closure
         private void button3_Click_1(object sender, EventArgs e)
         {
             
